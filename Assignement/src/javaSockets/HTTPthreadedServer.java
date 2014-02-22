34d942b4894e7e3d341a14f7ed8d1902848d6d70
Bruno Coussement <coussemetn.bruno@gmail.com>
2014-02-22 11:48:28 +0100

    dees is em

---
+++ Assignement/src/javaSockets/HTTPthreadedServer.java
@@ -0,0 +1,20 @@
+package javaSockets;
+import java.io.*;
+import java.net.*;
+
+public class HTTPthreadedServer {
+	public static void main(String argv[]) throws Exception
+	{
+	Socket clientSocket = new Socket("localhost", 6789);
+	while (true)
+	{
+	Socket clientSocket = clientSocket.accept();
+	if (connectionSocket != null)
+	{
+	Handler h = new Handler(clientSocket);
+	Thread thread = new Thread(request);
+	thread.start();
+	}
+	}
+	}
+}

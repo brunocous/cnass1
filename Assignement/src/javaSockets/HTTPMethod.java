a52d49f11d4ca4660c0e191028fcf30dba9bb39f
Bruno Coussement <coussemetn.bruno@gmail.com>
2014-02-26 00:14:41 +0100

    dfgh

--- Assignement/src/javaSockets/HTTPMethod.java
+++
@@ -1,5 +0,0 @@
-package javaSockets;
-
-public class HTTPMethod {
-
-}
--- Assignement/src/javaSockets/HTTPclient.java
+++ Assignement/src/javaSockets/HTTPclient.java
@@ -3,7 +3,7 @@ package javaSockets;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
-import httpmethod.HTTPMethod;
+import httpmethod.*;
 
 public class HTTPclient {
 	/**
@@ -26,6 +26,7 @@ public class HTTPclient {
 	 * An arraylist containing pending requests of this client.
 	 */
 	private ArrayList<HTTPMethod> requests;
+
 	/**
 	 * This constructor initiates an instance of HTTPclient. It creates a local
 	 * connection on port 6789.
@@ -62,82 +63,6 @@ public class HTTPclient {
 	}
 
 	/**
-	 * @return the inFromUser
-	 */
-	public BufferedReader getInFromUser() {
-		return inFromUser;
-	}
-
-	/**
-	 * @param inFromUser
-	 *            the inFromUser to set
-	 */
-	public void setInFromUser(BufferedReader inFromUser) {
-		this.inFromUser = inFromUser;
-	}
-
-	/**
-	 * @return the clientSocket
-	 */
-	public Socket getClientSocket() {
-		return clientSocket;
-	}
-
-	/**
-	 * @param clientSocket
-	 *            the clientSocket to set
-	 */
-	public void setClientSocket(Socket clientSocket) {
-		this.clientSocket = clientSocket;
-	}
-
-	/**
-	 * @return the outToServer
-	 */
-	public DataOutputStream getOutToServer() {
-		return outToServer;
-	}
-
-	/**
-	 * @param outToServer
-	 *            the outToServer to set
-	 */
-	public void setOutToServer(DataOutputStream outToServer) {
-		this.outToServer = outToServer;
-	}
-
-	/**
-	 * @return the inFromServer
-	 */
-	public BufferedReader getInFromServer() {
-		return inFromServer;
-	}
-
-	/**
-	 * @param inFromServer
-	 *            the inFromServer to set
-	 */
-	public void setInFromServer(BufferedReader inFromServer) {
-		this.inFromServer = inFromServer;
-	}
-
-	/**
-	 * Reads the user input using the buffered reader. Else return null.
-	 * 
-	 * @return The user input in String type.
-	 */
-	public String getUserCommand() {
-		try {
-			return this.getInFromUser().readLine();
-		} catch (IOException e) {
-			System.out
-					.println("Error. Something went wrong when reading user input.");
-			return null;
-		}
-
-	}
-
-	/**
 	 * Sends the given command over the connection associated with this
 	 * HTTPclient.
 	 * 
@@ -150,12 +75,35 @@ public class HTTPclient {
 		if (userCommandPieces == null) {
 			throw new IllegalCommandException();
 		}
-		
+		this.addNewRequest(userCommandPieces);
 		String approvedCommand = "";
 		for (int i = 0; i < userCommandPieces.length; i++) {
 			approvedCommand += userCommandPieces[i] + " ";
 		}
 		this.getOutToServer().writeBytes(approvedCommand + '\n');
+	}
+
+	private void addNewRequest(String[] userCommandPieces) {
+		int port = Integer.parseInt(userCommandPieces[2]);
+		HTTPVersion version;
+		if (userCommandPieces[3].equals(HTTPVersion.HTTP1point0.toString())) {
+			version = HTTPVersion.HTTP1point0;
+		}
+		if (userCommandPieces[3].equals(HTTPVersion.HTTP1point1.toString())) {
+			version = HTTPVersion.HTTP1point0;
+		} else
+			version = null;
+		switch (userCommandPieces[0]) {
+		case "GET":
+			this.addRequest(new Get(userCommandPieces[1], port, version));
+		case "POST":
+			this.addRequest(new Post(userCommandPieces[1], port, version));
+		case "PUT":
+			this.addRequest(new Put(userCommandPieces[1], port, version));
+		case "HEAD":
+			this.addRequest(new Head(userCommandPieces[1], port, version));
+		}
+
 	}
 
 	/**
@@ -261,23 +209,136 @@ public class HTTPclient {
 		}
 		return (String[]) filtered.toArray();
 	}
-	public void addRequest(HTTPMethod request){
-		if(isValidRequest(request) && !this.hasRequest(request))
+
+	/**
+	 * Adds the given request to the list of request of this client if and only
+	 * if the given request is a valid request and the list of requests doesn't
+	 * contain the given request.
+	 * 
+	 * @param request
+	 */
+	public void addRequest(HTTPMethod request) {
+		if (isValidRequest(request) && !this.hasRequest(request))
 			this.getRequests().add(request);
 	}
-	public boolean hasRequest(HTTPMethod request){
+
+	/**
+	 * True if and only if this client has the given request as one of its
+	 * requests.
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public boolean hasRequest(HTTPMethod request) {
 		return this.getRequests().contains(request);
 	}
-	public boolean isValidRequest(HTTPMethod request){
-		return (HTTPMethod.isValidUri(request.getUri()) 
-				&& HTTPMethod.isValidPort(request.getPort())
-				&& HTTPMethod.isValidHTTPVersion(request.getVersion()));
+
+	/**
+	 * True if and only if the given request has a valid URI and a valid port
+	 * and a valid HTTP Version.
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public boolean isValidRequest(HTTPMethod request) {
+		return (HTTPMethod.isValidUri(request.getUri())
+				&& HTTPMethod.isValidPort(request.getPort()) && HTTPMethod
+					.isValidHTTPVersion(request.getVersion()));
 	}
-	public void removeRequest(HTTPMethod request){
-		if(this.hasRequest(request))
+
+	/**
+	 * Removes the given request as one of this clients requests if and only if
+	 * this client has the given request as one of its requests.
+	 * 
+	 * @param request
+	 */
+	public void removeRequest(HTTPMethod request) {
+		if (this.hasRequest(request))
 			this.getRequests().remove(request);
 	}
-	public ArrayList<HTTPMethod> getRequests(){
+
+	/**
+	 * Returns the request of this client.
+	 * 
+	 * @return
+	 */
+	public ArrayList<HTTPMethod> getRequests() {
 		return this.requests;
+	}
+
+	/**
+	 * @return the inFromUser
+	 */
+	public BufferedReader getInFromUser() {
+		return inFromUser;
+	}
+
+	/**
+	 * @param inFromUser
+	 *            the inFromUser to set
+	 */
+	public void setInFromUser(BufferedReader inFromUser) {
+		this.inFromUser = inFromUser;
+	}
+
+	/**
+	 * @return the clientSocket
+	 */
+	public Socket getClientSocket() {
+		return clientSocket;
+	}
+
+	/**
+	 * @param clientSocket
+	 *            the clientSocket to set
+	 */
+	public void setClientSocket(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
+
+	/**
+	 * @return the outToServer
+	 */
+	public DataOutputStream getOutToServer() {
+		return outToServer;
+	}
+
+	/**
+	 * @param outToServer
+	 *            the outToServer to set
+	 */
+	public void setOutToServer(DataOutputStream outToServer) {
+		this.outToServer = outToServer;
+	}
+
+	/**
+	 * @return the inFromServer
+	 */
+	public BufferedReader getInFromServer() {
+		return inFromServer;
+	}
+
+	/**
+	 * @param inFromServer
+	 *            the inFromServer to set
+	 */
+	public void setInFromServer(BufferedReader inFromServer) {
+		this.inFromServer = inFromServer;
+	}
+
+	/**
+	 * Reads the user input using the buffered reader. Else return null.
+	 * 
+	 * @return The user input in String type.
+	 */
+	public String getUserCommand() {
+		try {
+			return this.getInFromUser().readLine();
+		} catch (IOException e) {
+			System.out
+					.println("Error. Something went wrong when reading user input.");
+			return null;
+		}
+
 	}
 }

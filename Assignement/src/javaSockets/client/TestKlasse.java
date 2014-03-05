c88e0e07fc8040dc5e1f38427022fedd63509528
Bruno Coussement <coussement.bruno@gmail.com>
2014-03-05 21:05:11 +0100

    sdfghjnk;l

--- Assignement/src/javaSockets/client/TestKlasse.java
+++ Assignement/src/javaSockets/client/TestKlasse.java
@@ -2,19 +2,44 @@ package javaSockets.client;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class TestKlasse {
-	//dfghjkl
-	
+	// dfghjkl
+
 	public static void main(String argv[]) {
-		String response = "<img onerror=\"img_onerror(this);\" data-logit=\"true\" data-pid=\"MOBDDDBRHVWQZHYY\"\n" + 
-				   "   data-imagesize=\"thumb\"\n" + 
-				   "   data-error-url=\"http://img1a.flixcart.com/mob/thumb/mobile.jpg\"\n" + 
-				   "   src=\"http://img8a.flixcart.com/image/mobile/h/y/y/samsung-galaxy-s-duos-s7562-125x125-imadddczzr4qhqnc.jpeg\"\n" + 
-				   "   alt=\"Samsung Galaxy S Duos S7562: Mobile\"\n" + 
-				   "   title=\"Samsung Galaxy S Duos S7562: Mobile\"></img></a>";
+		
+		int i = 0;
+		String response = "<img onerror=\"img_onerror(this);\" data-logit=\"true\" data-pid=\"MOBDDDBRHVWQZHYY\"\n"
+				+ "   data-imagesize=\"thumb\"\n"
+				+ "   data-error-url=\"http://img1a.flixcart.com/mob/thumb/mobile.jpg\"\n"
+				+ "   src=\"http://img8a.flixcart.com/image/mobile/h/y/y/samsung-galaxy-s-duos-s7562-125x125-imadddczzr4qhqnc.jpeg\"\n"
+				+ "   alt=\"Samsung Galaxy S Duos S7562: Mobile\"\n"
+				+ "   title=\"Samsung Galaxy S Duos S7562: Mobile\"></img></a>";
+		response += "<img onerror=\"img_onerror(this);\" data-logit=\"true\" data-pid=\"MOBDDDBRHVWQZHYY\"\n"
+				+ "   data-imagesize=\"thumb\"\n"
+				+ "   data-error-url=\"http://img1a.flixcart.com/mob/thumb/mobile.jpg\"\n"
+				+ "   src=\"httpHHHHHHHHHHHHHHHHHHHHHHHHH://img8a.flixcart.com/image/mobile/h/y/y/samsung-galaxy-s-duos-s7562-125x125-imadddczzr4qhqnc.jpeg\"\n"
+				+ "   alt=\"Samsung Galaxy S Duos S7562: Mobile\"\n"
+				+ "   title=\"Samsung Galaxy S Duos S7562: Mobile\"></img></a>";
 		Document doc = Jsoup.parse(response);
-		String resultaat = doc.getElementsByTag("img").attr("src");
-		System.out.println(resultaat);
+		Elements list = doc.getElementsByTag("img");
+		String[] urls = new String[list.size()];
+		for (Element element : list) {
+
+			String url = element.attr("src");
+			if(url !=  null){
+			urls[i] = url; 
+				i++;
+			}
+
+		}
+		String resul = "";
+		for (String el : urls) {
+			resul +=el + "\n";
+		}
+		System.out.println(resul);
+		System.out.println("tot hier");
 	}
 }

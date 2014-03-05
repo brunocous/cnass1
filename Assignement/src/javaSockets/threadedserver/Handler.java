b98e6ba83f59135d6ff0824de2430ed9377ddccd
Bruno Coussement <coussement.bruno@gmail.com>
2014-03-05 18:01:07 +0100

    hjk

--- Assignement/src/javaSockets/threadedserver/Handler.java
+++ Assignement/src/javaSockets/threadedserver/Handler.java
@@ -2,6 +2,9 @@ package javaSockets.threadedserver;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javaSockets.HTTPVersion;
 
 class Handler implements Runnable {
 	private Socket socket;
@@ -14,6 +17,11 @@ class Handler implements Runnable {
 
 	@Override
 	public void run() {
+		String[] commandPieces = parseCommand(getReader().readLine()
+				.toUpperCase());
+		processCommand(commandPieces);
+		if (Arrays.asList(commandPieces).contains("HTTP/1.0"))
+			quit();
 
 		String clientSentence = inFromClient.readLine();
 		System.out.println("Received: " + clientSentence);
@@ -22,21 +30,26 @@ class Handler implements Runnable {
 	}
 
 	/**
-	 * First checks if the given command is a valid command. If and only if the
-	 * given command is a valid command, it is sliced after a " ". 
-	 * If the given command is not a valid command, this method returns the null-object.
+	 * The given command is first sliced after a blank character. Then this
+	 * methods checks if the sliced command is a valid command. If the given
+	 * command is not a valid command, this method returns the null-object.
+	 * Otherwise the sliced command is returned.
 	 * 
 	 * @return
 	 */
 	public String[] parseCommand(String command) {
+		String[] pieces = filterBlanks(command.split(" "));
+		if (isValidCommand(pieces)) {
+			return pieces;
+		} else
+			return null;
+	}
+
+	public void processCommand(String[] command) {
 
 	}
-	
-	public void processCommand(String[] command){
-		
-	}
-	
-	public void quit(){
+
+	public void quit() {
 		this.setSocket(null);
 		this.setActive(false);
 	}
@@ -48,8 +61,8 @@ class Handler implements Runnable {
 	public void setSocket(Socket socket) {
 		this.socket = socket;
 	}
-	
-	public boolean isValidSocket(Socket socket){
+
+	public boolean isValidSocket(Socket socket) {
 		return false;
 	}
 
@@ -69,8 +82,8 @@ class Handler implements Runnable {
 	}
 
 	/**
-	 * Returns a buffered reader that is generated using the socket of this
-	 * handler.
+	 * Returns a buffered reader that reads the stream associated with the
+	 * socket of this handler.
 	 * 
 	 * @return
 	 * @throws IOException
@@ -89,6 +102,48 @@ class Handler implements Runnable {
 	 */
 	public DataOutputStream getOutputStream() throws IOException {
 		return new DataOutputStream(this.getSocket().getOutputStream());
+	}
+
+	/**
+	 * Iterates over the string array and deletes array elements only containing
+	 * a blank space.
+	 */
+	public static String[] filterBlanks(String[] arrayToFilter) {
+		ArrayList<String> filtered = new ArrayList<String>();
+		for (int i = 0; i < arrayToFilter.length; i++) {
+			if (!arrayToFilter[i].equals(" "))
+				filtered.add(arrayToFilter[i]);
+		}
+		return (String[]) filtered.toArray();
+	}
+
+	/**
+	 * Returns true if and only if the first part is a valid HTTP method, and
+	 * the second part a valid uri and the third part a valid port and the last
+	 * part a valid HTTP version.
+	 */
+	public static boolean isValidCommand(String[] command) {
+		return isValidHttpCommand(command[0]) && isValidUri(command[1])
+				&& isValidPort(command[2]) && isValidHttpVersion(command[3]);
+	}
+
+	private static boolean isValidHttpVersion(String string) {
+		return (string.equals("HTTP/1.0") || string.equals("HTTP/1.1"));
+	}
+
+	private static boolean isValidPort(String string) {
+		int port = Integer.parseInt(string);
+		return (port >= 0) && (port <=63535);
+	}
+
+	private static boolean isValidUri(String string) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static boolean isValidHttpCommand(String string) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }

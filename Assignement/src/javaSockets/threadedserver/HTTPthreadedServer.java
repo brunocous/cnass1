fd86430dd8151540cf40a6c48dd33b6eb5c5be96
Bruno Coussement <coussement.bruno@gmail.com>
2014-03-07 00:18:29 +0100

    threaded server af

--- Assignement/src/javaSockets/threadedserver/HTTPthreadedServer.java
+++ Assignement/src/javaSockets/threadedserver/HTTPthreadedServer.java
@@ -17,14 +17,29 @@ public class HTTPthreadedServer {
 	 * The socket of this server.
 	 */
 	private final ServerSocket serverSocket;
-	
-	public HTTPthreadedServer() throws IOException{
-		for(int i = 0; i < getNumberofhandlersinpool(); i++){
+
+	/**
+	 * Creates a new server with a fixed number of initial inactive handlers and
+	 * a connection on port 6789.
+	 * 
+	 * @throws IOException
+	 */
+	public HTTPthreadedServer() throws IOException {
+		for (int i = 0; i < getNumberofhandlersinpool(); i++) {
 			this.threadPool.add(new Handler());
 		}
 		serverSocket = new ServerSocket(6789);
 	}
-	
+
+	/**
+	 * First a new server is created. Then the server waits for an incoming
+	 * connection. If an connection is accepted, then that connection is
+	 * assigned to an inactive handler from the thread pool of the server. That
+	 * new active handler is then ran.
+	 * 
+	 * @param argv
+	 * @throws Exception
+	 */
 	public static void main(String argv[]) throws Exception {
 		HTTPthreadedServer server = new HTTPthreadedServer();
 		while (true) {
@@ -36,15 +51,24 @@ public class HTTPthreadedServer {
 		}
 	}
 
+	/**
+	 * Assigns the given connection to an inactive handler. If this server does
+	 * not have inactive handler, then a new handler is made and the given
+	 * connection is assigned to the new handler.
+	 * 
+	 * 
+	 * @param connection
+	 * @return The handler the given connection is assigned to.
+	 */
 	private Runnable assignToHandler(Socket connection) {
 		Handler newlyAssignedHandler = null;
-		for(Handler handler: this.getThreadPool()){
-			if(handler.canAcceptConnection()){
+		for (Handler handler : this.getThreadPool()) {
+			if (handler.canAcceptConnection()) {
 				handler.acceptConnection(connection);
 				newlyAssignedHandler = handler;
 			}
 		}
-		if(!this.hasInactiveHandler()){
+		if (!this.hasInactiveHandler()) {
 			newlyAssignedHandler = this.makeAndAssignHandler(connection);
 		}
 		return newlyAssignedHandler;
@@ -56,31 +80,40 @@ public class HTTPthreadedServer {
 	public static int getNumberofhandlersinpool() {
 		return numberOfHandlersInPool;
 	}
+
 	/**
 	 * Returns the pool of threads of this server.
 	 */
-	public ArrayList<Handler> getThreadPool(){
+	public ArrayList<Handler> getThreadPool() {
 		return this.threadPool;
 	}
+
 	/**
-	 * Adds a new handler to the pool of this server and assigns the given connection to it.
+	 * Adds a new handler to the pool of this server and assigns the given
+	 * connection to it.
 	 */
-	public Handler makeAndAssignHandler(Socket connection){
-		
+	public Handler makeAndAssignHandler(Socket connection) {
+		Handler newHandler = new Handler();
+		newHandler.acceptConnection(connection);
+		this.getThreadPool().add(newHandler);
+		return newHandler;
+
 	}
+
 	/**
 	 * Return the server socket of this server.
 	 */
-	public ServerSocket getServerSocket(){
+	public ServerSocket getServerSocket() {
 		return this.serverSocket;
 	}
+
 	/**
 	 * True if and only if this server has inactive handlers.
 	 */
-	public boolean hasInactiveHandler(){
+	public boolean hasInactiveHandler() {
 		boolean result = false;
-		for(Handler h: this.getThreadPool()){
-			if(h.canAcceptConnection())
+		for (Handler h : this.getThreadPool()) {
+			if (h.canAcceptConnection())
 				result = true;
 		}
 		return result;
